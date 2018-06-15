8460373a2b5f9bcce56e17c7a86bea14bfae8579
Brian <bstanley@gmail.com>
2018-06-15 15:58:00 -0500

    first commit

---
+++ deploy.R
@@ -0,0 +1,15 @@
+install.packages('rsconnect')
+library(rsconnect)
+
+shiny.apps.username <- Sys.getenv("SHINY_APPS_USERNAME")   
+shiny.apps.token <- Sys.getenv("SHINY_APPS_TOKEN")   
+shiny.apps.secret <- Sys.getenv("SHINY_APPS_SECRET")   
+
+rsconnect::setAccountInfo(name=shiny.apps.username,token=shiny.apps.token,secret=shiny.apps.secret)
+
+common.files <- list.files(path = "./common", pattern = "(\\.json$|\\.R$)", full.names = TRUE)
+
+for (folder in dir()[file.info(dir())$isdir]){
+  file.copy(common.files, folder)
+  rsconnect::deployApp(folder)
+}
